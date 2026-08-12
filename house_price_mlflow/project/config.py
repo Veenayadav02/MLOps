f1a7bae6f54a71ab589d9813f139f5a4b59bc12a
root <root@ip-172-31-16-130.ec2.internal>
2026-08-12 11:57:34 +0000

    Use MLflow tracking URI from environment

--- house_price_mlflow/project/config.py
+++ house_price_mlflow/project/config.py
@@ -1,5 +1,6 @@
 from urllib.parse import urlparse
 
-TRACKING_URI = "file:./mlruns"
+#TRACKING_URI = "file:./mlruns"
+TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
 EXPERIMENT_NAME = "HousePriceExperiment"
 MODEL_NAME = "Best Randomforest Model"
